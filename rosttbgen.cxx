5da452d2186db45b4ce5aa6f467c5fb5679e02b7
artemis-beta <krizar312@gmail.com>
2020-02-16 09:50:04 +0000

    Started file headers and fixed .pro file

--- rosttbgen.cxx
+++ rosttbgen.cxx
@@ -1,3 +1,32 @@
+//-------------------------------------------------------------------------//
+//         ROS Timetable Editor File Parser Class Definition               //
+//                                                                         //
+// This file provides part of the source code towards the standalone       //
+// timetable editor constructed using the Qt v5.14 framework.              //
+// for Railway Operation Simulator, a railway signalling simulation        //
+// by Albert Ball, Copyright (c) 2010 [original development].              //
+//                                                                         //
+// Copyright (C) 2020 Kristian Zarebski                                    //
+//                                                                         //
+// This program is free software: you can redistribute it and/or modify    //
+// it under the terms of the GNU General Public License as published by    //
+// the Free Software Foundation, either version 3 of the License, or       //
+// (at your option) any later version.                                     //
+//                                                                         //
+// This program is distributed in the hope that it will be useful,         //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of          //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           //
+// GNU General Public License for more details.                            //
+//                                                                         //
+// You should have received a copy of the GNU General Public License       //
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.   //
+//                                                                         //
+// Efforts to improve on this application are welcomed, with the request   //
+// that any improvements you deem notable be offered back to the           //
+// original project.                                                       //
+//                                                                         //
+//-------------------------------------------------------------------------//
+
 #include "rosttbgen.hxx"
 
 QString ROSTTBGen::parse_file(const QFileDialog* file, const QDir* directory)
